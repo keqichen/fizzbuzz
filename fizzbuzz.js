885bd1d94b6cab673c0a88dcc48fe8581b925f43
Keqi Chen <ckq009@gmail.com>
2023-01-16 16:07:17 +0000

    Add variables

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,23 @@ console.log("1");
 console.log("2");
 
 for(let i=1;i<=100;i++){
+    var string3 = "fizz";
+    var string5 = "buzz";
+    var string7 = "bang";
+    var string11 = "else";
     if (i%5==0 && i%3==0){
-        console.log("fizzbuzz")
+        console.log(string3+string5)
     }else if(i%5==0){
-        console.log("buzz")
+        console.log(string5)
     }else if(i%3==0){
-        console.log("fizz")
+        console.log(string3)
     }else if(i%7==0){
-        console.log("bang")
+        console.log(string7)
     }else if(i%11==0){
-        console.log("lala")
+        console.log(string11)
     }else{console.log(i);
     }
 }
+
+
 
